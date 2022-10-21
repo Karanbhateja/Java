75e5e136b22ee3a04eff4e011090acc9c95b24ca
Karan Bhateja <40824205+Karanbhateja@users.noreply.github.com>
2022-10-21 19:39:46 +0000

    Hello World

---
+++ A_Hello_World.java
@@ -0,0 +1,9 @@
+class A_Hello_World 
+{
+    public static void main(String [] args)
+    {
+        System.out.println("Hello world");
+        // Sout is shirt in IDEs 
+        // System is a class
+    }
+}
