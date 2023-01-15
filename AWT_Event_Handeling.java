5efec094f2bffec0171ac2ad8406f38c8731cc4d
Karan Bhateja <40824205+Karanbhateja@users.noreply.github.com>
2023-01-15 18:58:20 +0530

    Create AWT_Event_Handeling.java

---
+++ AWT_Event_Handeling.java
@@ -0,0 +1,35 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class AWT_Event_Handeling {
+    public static void main(String[] args) {
+
+/////////////////////////////// Frame & Label ////////////////////////////////////////
+
+        Frame f = new Frame();
+        f.setLayout(null);
+        f.setSize(700,600);
+        f.setTitle("Event Handeling");
+        f.setVisible(true);
+
+        Label l1 = new Label("Name");
+        l1.setBounds(20,40,80,30);
+
+/////////////////////////// Event Handeling //////////////////////////////////
+
+        TextField t1 = new TextField("Enter Name Here");
+        t1.setBounds(100,40,150,30);
+
+        Button b = new Button("Submit");
+        b.setBounds(80,100,50,30);
+        b.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                t1.setText("Hello");
+            }
+        });
+
+        f.add(t1);
+        f.add(b);
+    }
+}
